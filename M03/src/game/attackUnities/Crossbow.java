8d28ed08ae7bc0594cc940aae79f6b0a6ace1957
marmelich <150951688+marmelich@users.noreply.github.com>
2024-05-06 16:45:48 +0200

    añadiendo metodos de militaryunit

--- M03/src/game/attackUnities/Crossbow.java
+++ M03/src/game/attackUnities/Crossbow.java
@@ -3,16 +3,61 @@ package game.attackUnities;
 public class Crossbow extends AttackUnity{
 
 	//CONSTRUCTOR 1 por hacer
-		public Crossbow(int armor, int baseDamage) {
-			setArmor(ARMOR_CROSSBOW+(getTechnologyDefense()*PLUS_ARMOR_CROSSBOW_BY_TECHNOLOGY)*1000/100);
-			setBaseDAmage(BASE_DAMAGE_CROSSBOW+(getTechnologyAttack()*PLUS_ATTACK_CROSSBOW_BY_TECHNOLOGY)*1000/100);
-			setInitialArmor(getArmor());
-		}
+	public Crossbow(int armor, int baseDamage) {
+		setArmor(ARMOR_CROSSBOW+(getTechnologyDefense()*PLUS_ARMOR_CROSSBOW_BY_TECHNOLOGY)*1000/100);
+		setBaseDamage(BASE_DAMAGE_CROSSBOW+(getTechnologyAttack()*PLUS_ATTACK_CROSSBOW_BY_TECHNOLOGY)*1000/100);
+		setInitialArmor(getArmor());
+	}
 		
 	//CONSTRUCTOR 2
 	public Crossbow() {
 		setArmor(ARMOR_CROSSBOW); //armor por defecto
 		setBaseDamage(BASE_DAMAGE_CROSSBOW); //damage por defecto
 		
+	}
+	
+	
+	
+	//METODOS -----------
+
+	public int attack() {
+		return getBaseDamage();
+	}
+
+	public void takeDamage(int recievedDamage) {
+		setArmor(getArmor()-recievedDamage);
+		
+	}
+	
+	public int getActualArmor() {
+		return getArmor(); // ??? por qué no se usa getArmor?
+	}
+
+	public int getFoodCost() {
+		return FOOD_COST_CROSSBOW; //falta tener en cuenta incremetos de tecnlogia
+	}
+
+	public int getWoodCost() {
+		return WOOD_COST_CROSSBOW;
+	}
+
+	public int getIronCost() {
+		return IRON_COST_CROSSBOW;
+	}
+
+	public int getManaCost() {
+		return MANA_COST_CROSSBOW;
+	}
+
+	public int getChanceGeneratinWaste() {
+		return CHANCE_GENERATNG_WASTE_CROSSBOW;
+	}
+
+	public int getChanceAttackAgain() {
+		return CHANCE_ATTACK_AGAIN_CROSSBOW;
+	}
+
+	public void resetArmor() {
+		setArmor(getInitialArmor());
 	}
 }
--- M03/src/game/attackUnities/Swordsman.java
+++ M03/src/game/attackUnities/Swordsman.java
@@ -5,7 +5,7 @@ public class Swordsman extends AttackUnity{
 	//CONSTRUCTOR 1 por hacer
 	public Swordsman(int armor, int baseDamage) {
 		setArmor(ARMOR_SWORDSMAN+(getTechnologyDefense()*PLUS_ARMOR_SWORDSMAN_BY_TECHNOLOGY)*1000/100);
-		setBaseDAmage(BASE_DAMAGE_SWORDSMAN+(getTechnologyAttack()*PLUS_ATTACK_SWORDSMAN_BY_TECHNOLOGY)*1000/100);
+		setBaseDamage(BASE_DAMAGE_SWORDSMAN+(getTechnologyAttack()*PLUS_ATTACK_SWORDSMAN_BY_TECHNOLOGY)*1000/100);
 		setInitialArmor(getArmor());
 		
 	}
@@ -15,6 +15,50 @@ public class Swordsman extends AttackUnity{
 		setArmor(ARMOR_SWORDSMAN); //armor por defecto
 		setBaseDamage(BASE_DAMAGE_SWORDSMAN); //damage por defecto
 		
+	}
+	
+	
+	//METODOS -----------
+
+	public int attack() {
+		return getBaseDamage();
+	}
+
+	public void takeDamage(int recievedDamage) {
+		setArmor(getArmor()-recievedDamage);
+		
+	}
+	
+	public int getActualArmor() {
+		return getArmor(); // ??? por qué no se usa getArmor?
+	}
+
+	public int getFoodCost() {
+		return FOOD_COST_SWORDSMAN; //falta tener en cuenta incremetos de tecnlogia
+	}
+
+	public int getWoodCost() {
+		return WOOD_COST_SWORDSMAN;
+	}
+
+	public int getIronCost() {
+		return IRON_COST_SWORDSMAN;
+	}
+
+	public int getManaCost() {
+		return MANA_COST_SWORDSMAN;
+	}
+
+	public int getChanceGeneratinWaste() {
+		return CHANCE_GENERATNG_WASTE_SWORDSMAN;
+	}
+
+	public int getChanceAttackAgain() {
+		return CHANCE_ATTACK_AGAIN_SWORDSMAN;
+	}
+
+	public void resetArmor() {
+		setArmor(getInitialArmor());
 	}
 	
 }
