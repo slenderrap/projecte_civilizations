99df8d2aaa697cc16161f6a9799a45fdb45a19a1
orirribas2000@gmail.com <orirribas2000@gmail.com>
2024-05-09 21:12:04 +0200

    Intentando primeras conexiones a las base de datos

--- M03/src/bbdd/Datos.java
+++ M03/src/bbdd/Datos.java
@@ -1,5 +1,23 @@
 package bbdd;
 
-public class Datos {
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 
+public class Datos {
+	public static void main(String[] args) throws Exception{
+		String urlDatos="jdbc:oracle:thin:@192.168.56.1:1521:orcl";
+		String user = "civil";
+		String password = "civil";
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection conn =  DriverManager.getConnection(urlDatos,user,password);
+		Statement st =  conn.createStatement();
+		String query="Show tables";
+		ResultSet rs = st.executeQuery(query);
+		while (rs.next()) {
+			System.out.println(rs.getString(1));
+			
+		}
+	}
 }
